3b6afa8595e9d014f433cdd12b8c6a7c1457e77e
juntao.li <juntao.li@hkfdt.cn>
2018-07-18 09:24:40 +0800

    marketStatus
    update

--- StockFrame/UIFrameWork/NetWork/Network/DataManager/DataManager+data.swift
+++ StockFrame/UIFrameWork/NetWork/Network/DataManager/DataManager+data.swift
@@ -7,3 +7,6 @@
 //
 
 import Foundation
+
+extension DataManager {
+}
--- StockFrame/UIFrameWork/NetWork/Network/DataManager/DataManger+QuoteUpdate.swift
+++ StockFrame/UIFrameWork/NetWork/Network/DataManager/DataManger+QuoteUpdate.swift
@@ -7,3 +7,22 @@
 //
 
 import Foundation
+
+extension DataManager {
+    
+    
+    /// 保存 MarketStatus 状态
+    ///
+    /// - Parameter packet: 接收数据包
+    func handleMarketStatus(_ packet:JPacketReceiveBase) {
+        let pkt = packet as! MarketStatusUpdate
+        queue.async(flags: .barrier) {
+            var dic:[String:Int] = [:]
+            if self.marketStatueDic.keys.contains(pkt.ex) {
+                dic = self.marketStatueDic[pkt.ex]!
+            }
+            dic[pkt.sesname] = pkt.status
+            self.marketStatueDic[pkt.ex] = dic
+        }
+    }
+}
